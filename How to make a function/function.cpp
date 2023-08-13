c1429afca8d7430ee56d51c4d8bd5b4cf77a8805
AH <140490490+AHGh1386@users.noreply.github.com>
2023-08-13 11:29:12 +0330

    Update function.cpp

--- How to make a function/function.cpp
+++ How to make a function/function.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <math.h>
+float Plus(int,int);
 
+int main()
+{
+    int a=10;
+    float b=35;
+    Plus(a,b);
+    std::cout<<c;
+
+
+    return 0;
+}
+
+
+
+float Plus(int a,int b)
+{
+    int c;
+    c=a+b;
+    
+    return c;
+}
