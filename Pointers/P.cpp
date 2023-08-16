79d8a9005f42a976d00cca86d726ee308a498d04
AH <140490490+AHGh1386@users.noreply.github.com>
2023-08-16 12:10:27 +0330

    Create P.cpp

---
+++ Pointers/P.cpp
@@ -0,0 +1,11 @@
+string food = "Pasta";  // A food variable of type string
+string* ptr = &food;    // A pointer variable, with the name ptr, that stores the address of food
+
+// Output the value of food (Pasta)
+cout << food << "\n";
+
+// Output the memory address of food
+cout << &food << "\n";
+
+// Output the memory address of food with the pointer
+cout << ptr << "\n";
