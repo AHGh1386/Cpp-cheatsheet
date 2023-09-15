bc56e2ea6a5c5905d9971ab2fa30f2d3729aaba7
AH <140490490+AHGh1386@users.noreply.github.com>
2023-09-15 13:41:49 +0330

    Create FizzBuzz.cpp

---
+++ FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "FizzBuzz" << std::endl;
+        } else if (i % 3 == 0) {
+            std::cout << "Fizz" << std::endl;
+        } else if (i % 5 == 0) {
+            std::cout << "Buzz" << std::endl;
+        } else {
+            std::cout << i << std::endl;
+        }
+    }
+
+    return 0;
+}
